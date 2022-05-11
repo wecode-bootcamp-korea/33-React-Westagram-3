abb43b260182f3c72084f459b12d5bcb80a2adff
haeun <yoohaaeun@gmail.com>
2022-05-11 17:26:39 +0900

    초기 세팅 완료 2

---
+++ src/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// chulhye's component
+import LoginChulhye from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/chulhye/Login/Login.js";
+import MainChulhye from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/chulhye/Main/Main.js";
+
+// hyesu's component
+import LoginHyesu from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/hyesu/Login/Login.js";
+import MainHyesu from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/hyesu/Main/Main.js";
+
+// haeun's component
+import LoginHaeun from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/haeun/Login/Login.js";
+import MainHaeun from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/haeun/Main/Main.js";
+
+// eunbyul's compornent
+import LoginEunbyul from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/eunbyulcheon/Login/Login.js";
+import MainEunbyul from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/eunbyulcheon/Main/Main.js";
+
+// seongju's component
+import LoginSeongju from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/seongju/Login/Login.js";
+import MainSeongju from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/seongju/Main/Main.js";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    )
+}
