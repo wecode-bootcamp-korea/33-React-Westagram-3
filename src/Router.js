f0e162c1f35185770480931eeaf7d6ffb65a3b3e
haeun <yoohaaeun@gmail.com>
2022-05-11 18:52:54 +0900

    초기세팅 1차 수정

--- src/Router.js
+++ src/Router.js
@@ -1,31 +1,42 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // chulhye's component
-import LoginChulhye from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/chulhye/Login/Login.js";
-import MainChulhye from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/chulhye/Main/Main.js";
+import LoginChulhye from './pages/chulhye/Login/Login';
+import MainChulhye from './pages/chulhye/Main/Main';
 
 // hyesu's component
-import LoginHyesu from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/hyesu/Login/Login.js";
-import MainHyesu from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/hyesu/Main/Main.js";
+import LoginHyesu from './pages/hyesu/Login/Login';
+import MainHyesu from './pages/hyesu/Main/Main';
 
 // haeun's component
-import LoginHaeun from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/haeun/Login/Login.js";
-import MainHaeun from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/haeun/Main/Main.js";
+import LoginHaeun from './pages/haeun/Login/Login';
+import MainHaeun from './pages/haeun/Main/Main';
 
 // eunbyul's compornent
-import LoginEunbyul from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/eunbyulcheon/Login/Login.js";
-import MainEunbyul from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/eunbyulcheon/Main/Main.js";
+import LoginEunbyul from './pages/eunbyul/Login/Login';
+import MainEunbyul from './pages/eunbyul/Main/Main';
 
 // seongju's component
-import LoginSeongju from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/seongju/Login/Login.js";
-import MainSeongju from "/Users/eunbyulcheon/Desktop/wecode/westagram-project/src/pages/seongju/Main/Main.js";
+import LoginSeongju from './pages/seongju/Login/Login';
+import MainSeongju from './pages/seongju/Main/Main';
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-chulhye" element={<LoginChulhye />} />
+        <Route path="/main-chulhye" element={<MainChulhye />} />
+        <Route path="/login-hyesu" element={<LoginHyesu />} />
+        <Route path="/main-hyesu" element={<MainHyesu />} />
+        <Route path="/login-haeun" element={<LoginHaeun />} />
+        <Route path="/main-haeun" element={<MainHaeun />} />
+        <Route path="/login-eunbyul" element={<LoginEunbyul />} />
+        <Route path="/main-eunbyul" element={<MainEunbyul />} />
+        <Route path="/login-seongju" element={<LoginSeongju />} />
+        <Route path="/main-seongju" element={<MainSeongju />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/chulhye/Login/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <div>chulhello1</div>;
+};
+
+export default Login;
