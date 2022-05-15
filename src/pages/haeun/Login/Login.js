7efcc518d16c33a09e72b16c8fe8c3ac0de46a96
haeun <yoohaaeun@gmail.com>
2022-05-15 14:29:31 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/haeun/Login/Login.js
+++ src/pages/haeun/Login/Login.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
-import { AiFillFacebook } from 'react-icons/ai';
 import './Login.scss';
 
 function Login() {
+  const [idInput, setIdInput] = useState();
+  const [pwInput, setPwInput] = useState();
   const navigate = useNavigate();
 
   const goToMain = () => {
-    navigate('/main');
+    navigate('/main-haeun');
+  };
+
+  const handleIdInput = e => {
+    setIdInput(e.target.value);
+  };
+
+  const handlePwInput = e => {
+    setPwInput(e.target.value);
   };
 
   return (
     <main className="loginContainer">
       <h1>westagram</h1>
       <form>
-        <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input type="password" placeholder="비밀번호" />
+        <input
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={handleIdInput}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={handlePwInput}
+        />
         <button onClick={goToMain}>로그인</button>
       </form>
-      <a href="#">
-        <AiFillFacebook className="icon" />
+      <p>
+        <img src="" alt="" />
         <span>Facebook으로 로그인</span>
-      </a>
-      <a href="#">비밀번호를 잊으셨나요?</a>
+      </p>
+      <p>비밀번호를 잊으셨나요?</p>
     </main>
   );
 }
